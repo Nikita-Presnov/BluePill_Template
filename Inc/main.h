d2d9b9531311e8da36875742daa355aa87b20d66
Nikita <presnovnikita@yandex.ru>
2023-03-20 12:02:34 +0300

    USART2 Add

--- Inc/main.h
+++ Inc/main.h
@@ -4,7 +4,11 @@
 #include "stm32f1xx_ll_gpio.h"
 #include "stm32f1xx_ll_tim.h"
 #include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_usart.h"
+#include "stm32f1xx_ll_rcc.h"
 
 #define AUTORELOAD 8000
 #define PRESCALER  500
+#define BAUDRATE 9600
+#define FCLK 36000000
 #endif
--- Src/main.c
+++ Src/main.c
@@ -3,17 +3,39 @@
 int main()
 {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPAEN;
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_USART2EN;
+
 
     GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
     GPIOC->CRH |= GPIO_CRH_MODE13_0;
     
+    GPIOA->CRL |= 
+        GPIO_CRL_MODE2_1 | 
+        GPIO_CRL_CNF2_1 |
+        GPIO_CRL_MODE3_1 | 
+        GPIO_CRL_CNF3_1;
+    GPIOA->CRL &= ~(
+        GPIO_CRL_MODE2_0 | 
+        GPIO_CRL_CNF2_0 |
+        GPIO_CRL_MODE3_0 |
+        GPIO_CRL_CNF3_0
+    );
+    USART2->CR1 |= USART_CR1_TE | USART_CR1_RE;
+    USART2->CR1 &= ~(USART_CR1_PCE | USART_CR1_M);
+    USART2->CR2 &= USART_CR2_STOP;
+    // LL_RCC_GetSystemClocksFreq
+    // LL_USART_SetBaudRate()
+    // LL_USART_SetTransferDirection(USART2, LL_USART_DIRECTION_TX_RX);
+    USART2->BRR = (FCLK/(BAUDRATE*16) << 4) | FCLK%(BAUDRATE*16);
+    USART2->CR1 |= USART_CR1_RXNEIE;
+    USART2->CR1 |= USART_CR1_UE;
 
     TIM2->PSC = PRESCALER - 1;
     TIM2->ARR = AUTORELOAD - 1;
     TIM2->DIER |= TIM_DIER_UIE;
     TIM2->CR1 &= ~(TIM_CR1_DIR | TIM_CR1_CMS);
     TIM2->CR1 |= TIM_CR1_CEN;
+    NVIC_EnableIRQ(USART2_IRQn);
     NVIC_EnableIRQ(TIM2_IRQn);
     while(1);
 }
@@ -25,3 +47,8 @@ void TIM2_IRQHandler()
     GPIOC->BSRR = ((odr & (1 << 13)) << 16) | (~odr & (1 << 13));
 }
 
+void USART2_IRQHandler()
+{
+    USART2->SR &= ~USART_SR_RXNE;
+    uint32_t data = USART2->DR;
+}
