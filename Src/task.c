7a508dbcd9506552d038d4b4c20f3b2402a4728c
Nikita <presnovnikita@yandex.ru>
2023-05-18 13:11:16 +0300

    Redesign for this project

--- Src/task.c
+++ Src/task.c
@@ -1,78 +1,88 @@
+#include "stm32f1xx.h"
 #include "task.h"
 #define NULL ((void *)0)
 
-TASK task[TSK];
+TASK_TypeDef task[TSK];
 
 __IO uint32_t load_cpu;
 __IO uint32_t current_load;
 
-void task_stat();
+void TaskStat();
 
-void SysTick_Handler(void) {
-    for(char i=0; i<TSK; i++)
+void SysTick_Handler(void)
+{
+    for (uint8_t i = 0; i < TSK; i++)
     {
-        if (task[i].handler == NULL) break;
+        if (task[i].handler == NULL)
+            break;
 
         if (!task[i].counter)
         {
-            task[i].counter=task[i].period;
+            task[i].counter = task[i].period;
             task[i].handler();
             if (!task[i].loop)
-                remove_task(i);
+                RemoveTask(i);
             break;
-        } else
+        }
+        else
             task[i].counter--;
     }
-    current_load +=(72000-SysTick->VAL);
+    current_load += (72000 - SysTick->VAL);
 }
 
-void remove_task(uint8_t num) {
-    char i;
-    for(i=num; i<(TSK-2);i++) {
-        task[i]=task[i+1];
-    }
-    task[TSK-1].counter=0;
-    task[TSK-1].period=0;
-    task[TSK-1].loop=NOLOOP;
-    task[TSK-1].handler=NULL;
-}
-
-void add_task(  void (*callback)(void),
-                Task_Priority_TypeDef rank,
-                Task_Loop_TypeDef loop,
-                uint32_t period_ms)
+void RemoveTask(uint8_t num)
 {
-    char i=TSK-1;
-    do {
-        if (task[i-1].handler != NULL) {
-            task[i]=task[i-1];
+    for (uint8_t i = num; i < (TSK - 2); i++)
+    {
+        task[i] = task[i + 1];
+    }
+    task[TSK - 1].counter = 0;
+    task[TSK - 1].period = 0;
+    task[TSK - 1].loop = NOLOOP;
+    task[TSK - 1].handler = NULL;
+}
+
+void AddTask(void (*callback)(void),
+             Task_Priority_TypeDef rank,
+             Task_Loop_TypeDef loop,
+             uint32_t period_ms)
+{
+    uint8_t i = TSK - 1;
+    do
+    {
+        if (task[i - 1].handler != NULL)
+        {
+            task[i] = task[i - 1];
         }
         i--;
     } while (i);
-    task[i].loop=loop;
-    task[i].period=period_ms;
-    task[i].handler=callback;
-    task[i].counter=period_ms;
+    task[i].loop = loop;
+    task[i].period = period_ms;
+    task[i].handler = callback;
+    task[i].counter = period_ms;
 }
 
-void clear_task(void) {
-    for(char i=0;i<TSK;i++) {
-        task[i].loop=ENABLE;
-        task[i].period=0;
-        task[i].counter=0;
-        task[i].handler=NULL;
+void ClearTask(void)
+{
+    for (uint8_t i = 0; i < TSK; i++)
+    {
+        task[i].loop = ENABLE;
+        task[i].period = 0;
+        task[i].counter = 0;
+        task[i].handler = NULL;
     }
-    load_cpu=0;
-    current_load=0;
-    add_task(task_stat,TOP,LOOP,1000);
+    load_cpu = 0;
+    current_load = 0;
+    AddTask(TaskStat, TOP, LOOP, 1000);
 }
 
-
-uint32_t get_load_cpu() {
+uint32_t GetLoadCpu()
+{
     return load_cpu;
 }
 
-void task_stat() {
-    load_cpu=current_load;
-    current_load=0;
+void TaskStat()
+{
+    load_cpu = current_load;
+    current_load = 0;
 }
